22ae16b1c7e6a17310ca3a694f4075122bf09c22
fabianrios <hola@fabianrios.co>
2015-11-02 19:56:20 -0500

    initial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tinkeringApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tinkeringApp
+ */
+angular.module('tinkeringApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ web.js
@@ -0,0 +1,5 @@
+var gzippo = require('gzippo'),  express = require('express'), app = express();
+ 
+  app.use(express.logger('dev'));
+  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+  app.listen(process.env.PORT || 5000);
